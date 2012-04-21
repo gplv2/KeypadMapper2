7335808a380f05308cd98d31b739b67bd4e97c88
cobra <cobra_osm@yahoo.com>
2012-04-21 22:35:15 +0200

    removed unused parts, improved button handling

--- src/org/osm/keypadmapper2/KeypadMapper2Activity.java
+++ src/org/osm/keypadmapper2/KeypadMapper2Activity.java
@@ -12,20 +12,15 @@
      See the License for the specific language governing permissions and
      limitations under the License.
  */
-/* A user wants to use the application for capturing wildlife data. Leave his code inside
- * "if(wildlife)" statements until it's clear where his project is heading. */
 package org.osm.keypadmapper2;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.io.RandomAccessFile;
 import java.util.Date;
 import java.util.TimeZone;
 
-import junit.framework.Assert;
 import android.app.Activity;
 import android.content.Context;
 import android.location.Location;
@@ -48,8 +43,7 @@ final class Gps implements LocationListener {
 	public int record = 0, osmid = 0;
 	public TextView tw = null;
 	public RandomAccessFile out, osm;
-	public java.text.SimpleDateFormat f = new java.text.SimpleDateFormat(
-			"yyyy-MM-dd'T'HH:mm:ss");
+	public java.text.SimpleDateFormat f = new java.text.SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
 	public LocationManager lm;
 
 	public void onProviderDisabled(String provider) {
@@ -69,22 +63,15 @@ final class Gps implements LocationListener {
 		bearing = location.getBearing();
 		try {
 			out.seek(out.getFilePointer() - 24); // Overwrite </trkseg>\n</trk>\n</gpx>\n
-			out.writeBytes("<trkpt lat=\""
-					+ lat
-					+ "\" lon=\""
-					+ lon
-					+ "\">\n"
-					+ (location.hasAltitude() ? "<ele>"
-							+ (int) location.getAltitude() + "</ele>\n" : "")
-					+ "<time>"
-					+ (f.format(new java.util.Date(location.getTime())))
-					+ "Z</time>\n</trkpt>\n</trkseg>\n</trk>\n</gpx>\n");
+			out.writeBytes("<trkpt lat=\"" + lat + "\" lon=\"" + lon + "\">\n"
+					+ (location.hasAltitude() ? "<ele>" + (int) location.getAltitude() + "</ele>\n" : "") + "<time>"
+					+ (f.format(new java.util.Date(location.getTime()))) + "Z</time>\n</trkpt>\n</trkseg>\n</trk>\n</gpx>\n");
 		} catch (IOException e) {
 		}
 	}
 }
 
-public class KeypadMapper2Activity extends Activity implements OnClickListener { // implements LocationListener {
+public class KeypadMapper2Activity extends Activity implements OnClickListener {
 	final boolean wildlife = false;
 	private String val = "";
 	static Gps gps = null;
@@ -94,8 +81,7 @@ public class KeypadMapper2Activity extends Activity implements OnClickListener {
 			if (ViewGroup.class.isInstance(btnGroup.getChildAt(i))) {
 				SetButtons((ViewGroup) btnGroup.getChildAt(i));
 			} else if (Button.class.isInstance(btnGroup.getChildAt(i))) {
-				Button button = (Button) btnGroup.getChildAt(i);// findViewById
-																// (ids[i]);
+				Button button = (Button) btnGroup.getChildAt(i);
 				button.setOnClickListener(this);
 			} else if (ImageButton.class.isInstance(btnGroup.getChildAt(i))) {
 				ImageButton imageButton = (ImageButton) btnGroup.getChildAt(i);
@@ -104,60 +90,20 @@ public class KeypadMapper2Activity extends Activity implements OnClickListener {
 		}
 	}
 
-	private void Do(double fwd, double left) { // fwd and left are distances in
-												// 111111 meter
-		if (wildlife) {
-			PrintWriter out = null;
-			Date d = new Date();
-			try {
-				out = new PrintWriter(new FileOutputStream(
-						Environment.getExternalStorageDirectory()
-								+ "WildlifeSurvey", true));
-				out.println(d.getTime() + "," + gps.lon + "," + gps.lat + ","
-						+ val);
-			} catch (FileNotFoundException e) {
-				Assert.assertNotNull("Error writing the file!", out);
-			} finally {
-				if (out != null)
-					out.close();
-				Assert.assertNotNull("Error writing the file!", out);
-			}
+	private void Do(double fwd, double left) { // fwd and left are distances in 111111 meter
+		try {
+			gps.osm.seek(gps.osm.getFilePointer() - 7); // Overwrite </osm>\n
+			gps.osm.writeBytes("<node id='"
+					+ --gps.osmid
+					+ "' visible='true' lat='"
+					+ (gps.lat + Math.sin(Math.PI / 180 * gps.bearing) * left + Math.cos(Math.PI / 180 * gps.bearing) * fwd)
+					+ "' lon='"
+					+ (gps.lon + (Math.sin(Math.PI / 180 * gps.bearing) * fwd - Math.cos(Math.PI / 180 * gps.bearing) * left)
+							/ Math.cos(Math.PI / 180 * gps.lat)) + "'>\n <tag k='addr:housenumber' v='" + val + "'/>\n</node>\n</osm>\n");
 			val = "";
-		} else {
-			try {
-				gps.osm.seek(gps.osm.getFilePointer() - 7); // Overwrite
-															// </osm>\n
-				gps.osm.writeBytes("<node id='"
-						+ --gps.osmid
-						+ "' visible='true' lat='"
-						+ (gps.lat + Math.sin(Math.PI / 180 * gps.bearing)
-								* left + Math.cos(Math.PI / 180 * gps.bearing)
-								* fwd)
-						+ "' lon='"
-						+ (gps.lon + (Math.sin(Math.PI / 180 * gps.bearing)
-								* fwd - Math.cos(Math.PI / 180 * gps.bearing)
-								* left)
-								/ Math.cos(Math.PI / 180 * gps.lat))
-						+ "'>\n <tag k='addr:housenumber' v='" + val
-						+ "'/>\n</node>\n</osm>\n");
-				val = "";
-			} catch (IOException e) {
-				val = getString(R.string.errorFileOpen);
-			}
+		} catch (IOException e) {
+			val = getString(R.string.errorFileOpen);
 		}
-		/*
-		 * if (out == null) AlertDialog.Builder(mContext)
-		 * .setIcon(R.drawable.alert_dialog_icon)
-		 * .setTitle("Error writing file !")
-		 */
-		/*
-		 * .setPositiveButton(R.string.alert_dialog_ok, new
-		 * DialogInterface.OnClickListener() { public void
-		 * onClick(DialogInterface dialog, int whichButton) {
-		 * 
-		 * } });
-		 */
-		// .create ();
 	}
 
 	public void onClick(View v) {
@@ -167,19 +113,19 @@ public class KeypadMapper2Activity extends Activity implements OnClickListener {
 			gps.record = 0;
 			finish();
 		} else {
-			if (v == findViewById(R.id.button_C))
+			if (v == findViewById(R.id.button_C)) {
 				val = "";
-			else if (v == findViewById(R.id.button_DEL)) {
+			} else if (v == findViewById(R.id.button_DEL)) {
 				if (val.length() > 0)
 					val = val.substring(0, val.length() - 1);
+			} else if (v == findViewById(R.id.button_L)) {
+				Do(0, 25.0 / 111111);
+			} else if (v == findViewById(R.id.button_F)) {
+				Do(50.0 / 111111, 0);
+			} else if (v == findViewById(R.id.button_R)) {
+				Do(0, -25.0 / 111111);
 			} else
 				val = val + v.getTag();
-			if (v == findViewById(R.id.button_L))
-				Do(0, 25.0 / 111111);
-			else if (v == findViewById(R.id.button_F))
-				Do(50.0 / 111111, 0);
-			else if (v == findViewById(R.id.button_R))
-				Do(0, -25.0 / 111111);
 			gps.tw.setText(val);
 		}
 	}
@@ -195,8 +141,7 @@ public class KeypadMapper2Activity extends Activity implements OnClickListener {
 		else {
 			gps = new Gps();
 			PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
-			gps.wl = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,
-					"WildlifeSurvey");
+			gps.wl = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "WildlifeSurvey");
 			gps.wl.acquire();
 			gps.tw = (TextView) findViewById(R.id.text);
 			gps.out = null;
@@ -204,22 +149,22 @@ public class KeypadMapper2Activity extends Activity implements OnClickListener {
 			gps.lm = (LocationManager) getSystemService(LOCATION_SERVICE);
 			try {
 				File sd = Environment.getExternalStorageDirectory();
-				if (!wildlife) {
-					gps.osm = new RandomAccessFile(new File(sd, "/"
-							+ (new Date()).getTime() + ".osm"), "rw");
-					gps.osm.writeBytes("<?xml version='1.0' encoding='UTF-8'?>\n"
-							+ "<osm version='0.6' generator='KeypadMapper'>\n</osm>\n");
-				}
-				gps.out = new RandomAccessFile(new File(sd, "/"
-						+ (new Date()).getTime() + ".gpx"), "rw");
-				gps.out.writeBytes("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
-						+ "<gpx\n"
-						+ "version=\"1.0\"\n"
-						+ "creator=\"KeypadMapper\"\n"
-						+ "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n"
-						+ "xmlns=\"http://www.topografix.com/GPX/1/0\"\n"
-						+ "xsi:schemaLocation=\"http://www.topografix.com/GPX/1/0 http://www.topografix.com/GPX/1/0/gpx.xsd\">\n"
-						+ "<trk>\n" + "<trkseg>\n</trkseg>\n</trk>\n</gpx>\n");
+				gps.osm = new RandomAccessFile(new File(sd, "/" + (new Date()).getTime() + ".osm"), "rw");
+				gps.osm.writeBytes("<?xml version='1.0' encoding='UTF-8'?>\n" + "<osm version='0.6' generator='KeypadMapper'>\n</osm>\n");
+				gps.out = new RandomAccessFile(new File(sd, "/" + (new Date()).getTime() + ".gpx"), "rw");
+				gps.out.writeBytes(
+						"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" + 
+						"<gpx\n" + 
+						"version=\"1.0\"\n" + 
+						"creator=\"KeypadMapper\"\n" + 
+						"xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n" + 
+						"xmlns=\"http://www.topografix.com/GPX/1/0\"\n"	+ 
+						"xsi:schemaLocation=\"http://www.topografix.com/GPX/1/0 http://www.topografix.com/GPX/1/0/gpx.xsd\">\n" + 
+						"<trk>\n" + 
+						"<trkseg>\n" +
+						"</trkseg>\n" +
+						"</trk>\n" +
+						"</gpx>\n");
 			} catch (FileNotFoundException e) {
 				gps.tw.setText(R.string.errorFileOpen);
 				// Assert.assertNotNull ("Error writing the file!", gps.out);
@@ -236,8 +181,7 @@ public class KeypadMapper2Activity extends Activity implements OnClickListener {
 		super.onResume();
 		if (gps.record == 0) {
 			gps.wl.acquire();
-			gps.lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000,
-					6f, gps);
+			gps.lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 6f, gps);
 			gps.record = 1;
 			gps.lon = -200;
 		}
